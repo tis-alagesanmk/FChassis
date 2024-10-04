037b96d0a77bb8df903348de470a3f60abd496a7
tis-alagesanmk <alagesan.m.k@teckinsoft.in>
2024-10-04 17:21:20 +0530

    sample code for SettingPanels

--- FChassis.UI/SettingPanels/Panel.axaml.cs
+++ FChassis.UI/SettingPanels/Panel.axaml.cs
@@ -1,4 +1,17 @@
+using System.Windows.Documents;
+
 namespace FChassis.UI.Settings;
 public partial class Panel : FChassis.UI.Panels.Child {
+
+   /*class TabItem {
+      internal Avalonia.Controls.TabItem tabItem;
+      internal Panel panel;
+   }
+   class TabItemList : List<TabItem> {
+   }
+   void PopulateTab(TabItemList itemList) {
+      TabItem ti  = null;
+      ti.tabItem.Content = ti.panel;
+   }*/
   
 }
