fedd56ea5124255cf8113a66cc34e6be6b1c4c93
samyraj <samyraj1989@gmail.com>
2024-10-08 17:44:45 +0530

    Samyraj UI Techparams

---
+++ FChassis.UI/SettingPanels/Machine/TechParams/AnalogScaling.axaml.cs
@@ -0,0 +1,5 @@
+
+namespace FChassis.UI.Settings.Machine.TechParams;
+
+public partial class AnalogScaling : FChassis.UI.Settings.Panel {
+}
--- FChassis.UI/SettingPanels/Machine/TechParams/Panel.axaml.cs
+++ FChassis.UI/SettingPanels/Machine/TechParams/Panel.axaml.cs
@@ -1,3 +1,15 @@
+using Avalonia.Markup.Xaml;
+
 namespace FChassis.UI.Settings.Machine.TechParams;
-public partial class Panel : FChassis.UI.Settings.Panel {
+public partial class Panel : FChassis.UI.Settings.TabPanel {
+   public Panel () {
+      AvaloniaXamlLoader.Load (this);
+      this.PopulateTabItemContent ([this.analogScaling, null, null, this.exhaustSystem,null]);
+   }
+
+
+   #region "Fields"
+   TechParams.AnalogScaling analogScaling = new ();
+   TechParams.ExhaustSystem exhaustSystem = new ();
+   #endregion "Fields"
 }
