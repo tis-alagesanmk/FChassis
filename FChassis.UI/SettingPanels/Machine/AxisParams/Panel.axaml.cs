312eccb64666af615c95c018262d1b4572193dde
samyraj <samyraj1989@gmail.com>
2024-10-08 14:46:36 +0530

    1.Added missing styles and label for HMI and Machine setting 2.Added new panel Xaxis for Axis Params

--- FChassis.UI/SettingPanels/Machine/AxisParams/Panel.axaml.cs
+++ FChassis.UI/SettingPanels/Machine/AxisParams/Panel.axaml.cs
@@ -1,3 +1,15 @@
+using Avalonia.Markup.Xaml;
+
 namespace FChassis.UI.Settings.Machine.AxisParams;
-public partial class Panel : FChassis.UI.Settings.Panel {
+public partial class Panel : FChassis.UI.Settings.TabPanel {
+
+   public Panel () {
+      AvaloniaXamlLoader.Load (this);
+      this.PopulateTabItemContent ([this.xAxis]);
+   }
+
+
+   #region "Fields"
+   AxisParams.XAxis xAxis = new ();
+   #endregion "Fields"
 }
