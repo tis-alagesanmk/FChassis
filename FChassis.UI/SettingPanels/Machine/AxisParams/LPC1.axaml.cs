aa9ea4b7c1fd3872e665f3c9e647ba32e8e11fae
Hemanth <hemanth.c@teckinsoft.in>
2024-10-10 10:44:15 +0530

    Panels UI moved to respective files Removed unused files

--- FChassis.UI/SettingPanels/Machine/AxisParams/LPC1.axaml.cs
+++
@@ -1,8 +0,0 @@
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
-
-namespace FChassis.UI.Settings.Machine.AxisParams;
-
-public partial class LPC1 : FChassis.UI.Settings.Panel {
-}
--- FChassis.UI/SettingPanels/Machine/PLCOptions/FunctionParameters.axaml.cs
+++
@@ -1,8 +0,0 @@
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
-
-namespace FChassis.UI.Settings.Machine.PLCOptions;
-
-public partial class FunctionParameters : FChassis.UI.Settings.Panel {
-}
