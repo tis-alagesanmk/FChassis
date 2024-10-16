572fc411f66f392e865c688724bd0868d9d37d76
Hemanth <hemanth.c@teckinsoft.in>
2024-10-16 13:01:48 +0530

    Added Laser Cutting dataBase

---
+++ FChassis.UI/SettingPanels/Laser/ViewModel/LaserCuttingViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FChassis.UI.SettingPanels.Laser.ViewModel {
+   public class LaserCuttingViewModel {
+      //public static CuttingVM = new LaserCuttingViewModel()
+   }
+}
