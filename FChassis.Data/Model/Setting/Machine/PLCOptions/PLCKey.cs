eedad965ae69d207c64101ad00056dd29e957ebd
samyraj42 <45854906+samyraj42@users.noreply.github.com>
2024-10-28 11:58:25 +0530

    Added model and viewmodel class for plckey

---
+++ FChassis.Data/Model/Setting/Machine/PLCOptions/PLCKey.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FChassis.Data.Model.Setting.Machine.PLCOptions {
+   public partial class PLCKey : ObservableObject{
+      [ObservableProperty,Prop(Prop.Type.Text,"Number",null,"","Name")]
+      private string? name;
+
+   }
+}
---
+++ FChassis.Data/ViewModels/Setting/Machine/PLCOptions/PLCKeyViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FChassis.Data.Model;
+using FChassis.Data.Model.Setting.Machine.PLCOptions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FChassis.Data.ViewModels.Setting.Machine.PLCOptions {
+   public partial class PLCKeyViewModel : PLCKey {
+
+      [ObservableProperty, Prop (Prop.Type.DGrid,null,null, "PLCKeys")]
+      private ObservableCollection<PLCKey> pLCKeys;
+
+      public PLCKeyViewModel () {
+         this.pLCKeys = new ObservableCollection<PLCKey> ();
+
+         this.pLCKeys.Add (new PLCKey () {Name = "Test" });
+      }
+   }
+}
